d2366336f64bde6a78045327f718c0fe9559b7f4
Ange230700 <kouakouangeericstephane@gmail.com>
2024-12-17 09:47:48 +0100

    refactor: 💡  finished refactorization and described current project state

--- javascript/dom/manipulation.js
+++ javascript/dom/manipulation.js
@@ -1,1 +1,17 @@
 // javascript\dom\manipulation.js
+
+import { decode, encode } from "../helpers/functions";
+
+function printMorseCode() {
+  document.querySelector("#morse-input").value = encode(
+    document.querySelector("#latin-input").value,
+  );
+}
+
+function printLatinText() {
+  document.querySelector("#latin-input").value = decode(
+    document.querySelector("#morse-input").value,
+  );
+}
+
+export { printMorseCode, printLatinText };
